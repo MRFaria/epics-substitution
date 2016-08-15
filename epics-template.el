a0f87e98675c4dc0a4fc912ab4d3801421f91d7b
Mauro <mauro.faria@diamond.ac.uk>
2016-08-15 09:55:50 +0000

    modified comments and added copyright

--- epics-template.el
+++ epics-template.el
@@ -1,38 +1,20 @@
+;; Copyright 2016 Mauro Faria
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (require 'org-table)
 
-;; (defun epics-template-mode-syntax-table (table &rest list)
-;;   "Copy TABLE and set syntax for successive CHARs according to strings S."
-;;   (setq table (copy-syntax-table table))
-;;   (while list
-;;     (modify-syntax-entry (pop list) (pop list) table))
-;;   table)
-
-;; (defvar epics-template-mode-syntax-table
-;;   (epics-template-mode-syntax-table ()
-;; 	?\# "< b"
-;; 	?\n "># b"
-
-;; 	?\' "\"'"
-;; 	?\` "\"`"
-;; 	;; ?$ might also have a ". p" syntax. Both "'" and ". p" seem
-;; 	;; to work fine. This is needed so that dabbrev-expand
-;; 	;; $VARNAME works.
-;; 	?$ "'"
-;; 	?! "_"
-;; 	?% "_"
-;; 	?: "_"
-;; 	?. "_"
-;; 	?^ "_"
-;; 	?~ "_"
-;; 	?, "_"
-;; 	?= "."
-;; 	?\; "."
-;; 	?| "."
-;; 	?& "."
-;; 	?< "."
-;; 	?> ".")
-;;   "The syntax table to use for Shell-Script mode.
-;; This is buffer-local in every such buffer.")
 
 (defvar epics-template-mode-syntax-table nil)
 (defvar my-highlights nil)
@@ -43,30 +25,21 @@
         (modify-syntax-entry ?# "< b" synTable)
         (modify-syntax-entry ?\n "> b" synTable)
         (modify-syntax-entry ?$ "'" synTable)
-
         synTable))
-;; (setq my-highlights
-;;       '(("record" . font-lock-function-name-face)
-;;         ("field" . font-lock-keyword-face)))
+
 (setq my-highlights
       '(
-        ;;horrible to look at but matches $() or ${} macro style
+        ;;horrible to look at, but matches $() or ${} style macros, might separate if it gets worse
         ("\\(\\$([a-zA-Z0-9.=_-]+)\\)\\|\\(\\${[a-zA-Z0-9.=_-]+}\\)" 0 font-lock-variable-name-face t)
         ("field" . font-lock-keyword-face)
         ("record" . font-lock-function-name-face)
         ))
-
-
 
 ;;;###autoload
 (define-derived-mode epics-template-mode fundamental-mode "template"
   (setq font-lock-defaults '(my-highlights))
   (visual-line-mode 0)
   (setq truncate-lines t))
-
-;; (font-lock-add-keywords 'epics-template-mode
-;;                          '("name" 0 font-lock-keyword-face t))
-
 
 (provide 'epics-template)
 
