3dbedf816a869e8813d9aaa4acdaf517a0ff8482
M.R.Faria <mauro.faria@diamond.ac.uk>
2016-09-19 12:11:48 +0100

    got rid of old functions that created a separate org table underneath the substitution table

--- epics-substitution.el
+++ epics-substitution.el
@@ -85,56 +85,34 @@
           (message "Export done."))
       (user-error "TABLE_EXPORT_FORMAT invalid"))))
 
-
-
-(defun find-commas (start end)
-   (save-excursion
-    (goto-char start)
-    (let (matches)
-      (while (< (point) end)
-        (cond ((= (char-after) ?,)
-               (push (point) matches)
-               (forward-char))
-              ((looking-at "[]\\[{}()]")
-               (forward-char))
-              (t
-               (forward-sexp))))
-      (nreverse matches))))
-
 (defun find-char (start end char)
   "looks for characters outside of strings"
   (interactive "r")
-  (print "test")
   (goto-char start)
   (let (matches)
     (while (< (point) end)
       (if (equal (char-after) char)
           (if (nth 3 (syntax-ppss (point)))
-              (print "in string")
+              (message "in-string")
             (push (point) matches)))
       (forward-char))
     (nreverse matches)))
 
 (defun replace-chars (beg end sep char)
   "replaces characters given a list of positions"
-  (interactive "r")
   (let ((char-list (find-char beg end char)))
-    (print char-list)
     (dolist (point char-list)
       (goto-char point)
       (delete-char 1)
       (insert sep))))
 
-
 (defun just-one-space-in-region (start end)
   "replace all whitespace before and after separator"
-  (interactive "r")
   (goto-char start)
   (catch 'exit-function
     (while (< (point) end)
       (if (equal (char-after) ?,)
           (if (nth 3 (syntax-ppss (point)))
-              (print "in string")
             (just-one-space) (forward-char) (just-one-space) (forward-char -2)))
       (condition-case nil
           (forward-char)
@@ -142,7 +120,6 @@
 
 (defun format-table (beg end)
   "formats table layour to keep a constant look"
-  (interactive "r")
   (goto-char beg)
   (cond ((search-forward-regexp "pattern[ ]?+{" end t )
            (forward-char -1)
@@ -225,137 +202,6 @@
         (just-one-space-in-region (point-min) (point-max))
         (align-table (point-min) (point-max))
         ))))
-;; (defun find-commas (start end)
-;;   (save-excursion
-;;     (goto-char start)
-;;     (let (matches)
-;;       (while (< (point) end)
-;;         (cond ((= (char-after) ?,)
-;;                (push (point) matches)
-;;                (forward-char))
-;;               ((looking-at "[]\\[{}()]")
-;;                (forward-char))
-;;               (t
-;;                (forward-sexp))))
-;;       (nreverse matches))))
-
-;;;###autoload
-(defun substitution-table-convert-region (beg0 end0 &optional separator)
-   ;; This function is only slightly modified code from org-table
-   "Convert region to a table.
-The region goes from BEG0 to END0, but these borders will be moved slightly, 
-to make sure a beginning of line in the first line is included.
-
-SEPARATOR specifies the field separator in the lines.  It can have the following values:
-
-'(4)     Use the comma as a field separator
-'(16)    Use a TAB as field separator
-integer  When a number, use that many spaces as field separator
-nil      When nil, the command tries to be smart and figure out the
-          separator in the following way:
-          - when each line contains a TAB, assume TAB-separated material
-          - when each line contains a comma, assume CSV material
-          - else, assume one or more SPACE characters as separator."
-   (interactive "rP")
-   (unwind-protect
-       (let ((debug-on-error t) (comma-list nil))
-         (with-demoted-errors "Error: %S"
-             ;; fundamental mode avoids issue
-             ;; with subtitution-mode-comments
-             (fundamental-mode)
-           (let* ((beg (min beg0 end0))
-                  (end (max beg0 end0))
-                  (beg-and-end (setup-table beg end))
-                  re)
-             (setq beg (pop beg-and-end))
-             (setq end (pop beg-and-end))
-             (goto-char beg)
-             (beginning-of-line 1)
-             (setq beg (point-marker))
-             (goto-char end)
-             (if (bolp) (backward-char 1) (end-of-line 1))
-             (setq end (point-marker))
-             ;; Get the right field separator
-             (unless separator
-               (setq separator '(4)))
-             (goto-char beg)
-             ;; Replace commas outside quotes
-             ;; with pipess
-             (setq comma-list (find-commas beg end))
-             (dolist (comma comma-list)
-               (goto-char comma)
-               (delete-char 1)
-               (insert "|"))
-             (goto-char beg)
-             (if (equal separator '(4))
-                 (while (< (point) end)
-                   (cond
-                    ((looking-at "^") (insert "| "))
-                    ((looking-at "[ \t]*$") (replace-match " |") (beginning-of-line 2))
-                    ((looking-at "[^,\n]+") (goto-char (match-end 0)))
-                    ;; moves down one line
-                    (t (beginning-of-line 2)))))
-             (goto-char beg)
-             (org-table-align))
-           (org-table-begin)
-           (org-table-insert-hline)
-           (orgtbl-send-table)
-           (orgtbl-toggle-comment)))
-         
-         ;; This always runs, even when errors occur above
-         (epics-substitution-mode)))
-
-
-(defun setup-table (beg end)
-  (goto-char beg)
-  (re-search-forward substitution--regex-string end)
-  (let ((name (match-string 2))
-        (orgtbl-gubbins))
-    (end-of-line)
-    (goto-char (1+ (point)))
-    (setq orgtbl-gubbins (concat "#+BEGIN RECEIVE ORGTBL " name
-                                 "\n#+END RECEIVE ORGTBL " name
-                                 "\n\n#+ORGTBL: SEND " name
-                                 " orgtbl-to-substitution :no-escape t\n"))
-    (insert orgtbl-gubbins)
-    (setq beg (point))
-    (setq end (+ end (length orgtbl-gubbins)))
-    (re-search-forward "{\\s-*pattern\\s-*{" end t)
-    (setq end (- end (length (match-string 0))))
-    (replace-match "" t t)
-    (while (re-search-forward "{\\|}" (1+ end) t)
-      (replace-match "")
-      (setq end (- end 1))))
-  (goto-char beg)
-  (while (search-forward "|" (1+ end) t)
-    (replace-match "\\vert" t t)
-    (setq end (+ end 4)))
-  (list beg end))
-
-
-;;;###autoload
-(defun substitution-table-convert-file (buffer)
-  (interactive "b")
-  (set-buffer buffer)
-  (goto-char (point-min))
-  (let (braces start end)
-    (while (re-search-forward substitution--regex-string (point-max) t)
-      (if (org-in-commented-line) () 
-        (beginning-of-line)
-        (setq start (point))
-        (search-forward "{")
-        (setq braces 1) ; Just passed the first brace so start at 1
-        (while (>= braces 1)
-          (if (char-equal (char-after) ?{)
-              (setq braces (1+ braces))
-            (if (char-equal (char-after) ?})
-                (setq braces (- braces 1))))
-          (forward-char)
-          )
-        (backward-char)
-        (setq end (point))
-        (substitution-table-convert-region start (point))
-        (goto-char end)))))
 
 ;;;###autoload
 (defun orgtbl-to-substitution (table params)
@@ -371,36 +217,14 @@ nil      When nil, the command tries to be smart and figure out the
      "\\\\vert" "|"
      (replace-regexp-in-string
       ", }" ", \"\"}"
-      (replace-regexp-in-string
-       "\\( ,\\)" " \"\","
+      ;; (replace-regexp-in-string
+      ;;  "\\( ,\\)" " \"\","
        (concat 
                (orgtbl-to-generic 
                 table (org-combine-plists params2 params)))
-       nil nil 1))
+       nil nil 1)
      :literal t)))
 
-;;;###autoload
-(defun orgtbl-to-substitution-old (table params)
-  "Convert the Orgtbl mode TABLE to substitution file syntax."
-  (let* ((params2
-          (list
-           :no-escape t
-           :tend "}"
-           :lstart " {"
-           :lend "}"
-           :sep ", ")))
-    ;; Convert \vert to | and put "" in blank cells
-    (replace-regexp-in-string
-     "\\\\vert" "|"
-     (replace-regexp-in-string
-      ", }" ", \"\"}"
-      (replace-regexp-in-string
-       "\\( ,\\)" " \"\","
-       (concat "{\npattern\n"
-               (orgtbl-to-generic 
-                table (org-combine-plists params2 params)))
-       nil nil 1))
-     :literal t)))
 
 (defun insert-templates (buffer &optional templates)
   (set-buffer buffer)
@@ -419,7 +243,6 @@ nil      When nil, the command tries to be smart and figure out the
 
 (defun read-template-macros (filename);; paths)
   "Read template file and include and return all macros."
-  (message "got here")
   (let ((macros '())
         (templates substitution--templates))
     (save-current-buffer
@@ -535,33 +358,6 @@ nil      When nil, the command tries to be smart and figure out the
             macros)
       (concat docs "\n"))))
 
-;;;###autoload
-(defun substitution-table-from-template-old ()
-  (interactive)
-  (if (not substitution--templates)
-      (call-interactively 'substitution-get-template-macros))
-  (let* ((template (completing-read "Select a template: "
-                                    substitution--templates))
-         (macros (read-template-macros (gethash template
-                                                substitution--templates))))
-    (insert (substitution-get-docs-from-template
-             (gethash template substitution--templates)))
-    (insert (concat "file " template
-                    "\n#+ BEGIN RECEIVE ORGTBL " template
-                    "\n#+ END RECEIVE ORGTBL " template
-                    "\n\n#+ORGTBL: SEND " template
-                    " orgtbl-to-substitution-old :no-escape t\n"))
-    (org-table-create (concat
-                       (number-to-string (length macros)) "x2"))
-    (forward-char)
-    (dolist (heading macros)
-      (if (not (equal (compare-strings heading 0 2 "__" nil nil) t))
-          (progn (insert heading)
-                 (org-table-next-field))
-        (org-table-delete-column))))
-  (org-table-align)
-  (org-table-delete-column))
-
 (defun substitution-table-from-template ()
   (interactive)
   (if (not substitution--templates)
@@ -637,6 +433,22 @@ nil      When nil, the command tries to be smart and figure out the
 (setq epics-substitution-mode-highlights
       '(("file\\|pattern" . font-lock-function-name-face)))
 
+(defun scroll-template-up ()
+  (interactive)
+  (search-backward "file" (point-min) t)
+  (move-beginning-of-line nil))
+
+(defun scroll-template-down ()
+  (interactive)
+  ;; check if we are in a comment
+  (while (or (nth 4 (syntax-ppss)) (looking-at "#"))
+      (forward-char))
+  (if (looking-at "file")
+      (end-of-line nil))
+  (search-forward "file" (point-max) t)
+  (move-beginning-of-line nil))
+  
+
 ;;;###autoload
 (define-derived-mode epics-substitution-mode fundamental-mode "epics-substitution"
   (setq comment-start "#")
@@ -645,6 +457,8 @@ nil      When nil, the command tries to be smart and figure out the
   (visual-line-mode 0)
   (setq truncate-lines t)
   (local-set-key (kbd "C-c #") 'orgtbl-toggle-comment)
-  (global-set-key (kbd "C-c C-f") 'substitution-convert-table))
+  (local-set-key (kbd "C-c C-f") 'substitution-convert-table)
+  (local-set-key (kbd "M-n") 'scroll-template-down)
+  (local-set-key (kbd "M-p") 'scroll-template-up))
 
 (provide 'epics-substitution)
